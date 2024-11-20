e8c184fbf670375058d6bd7ee062f52aff9584b6
Catronater <catronater@outlook.com>
2024-11-20 14:41:30 -0500

    Added something to show next turn

--- Contect4.py
+++ Contect4.py
@@ -8,27 +8,35 @@ def checkWin():
             if (x < 4):
                 if(arr[x][y]['text']==arr[x+1][y]['text']==arr[x+2][y]['text']==arr[x+3][y]['text'] and arr[x][y]['text']!= ''):
                     if(arr[x][y]['text'] == "Red"):
+                        playerturn.config(text = "Reset")
                         winLbl.config(text = "Red Wins!")
                     else:
+                        playerturn.config(text = "Reset")
                         winLbl.config(text = "Yellow Wins!")
             if (y < 4):
                 if(arr[x][y]['text']==arr[x][y+1]['text']==arr[x][y+2]['text']==arr[x][y+3]['text'] and arr[x][y]['text']!= ''):
                     if(arr[x][y]['text'] == "Red"):
+                        playerturn.config(text = "Reset")
                         winLbl.config(text = "Red Wins!")
                     else:
+                        playerturn.config(text = "Reset")
                         winLbl.config(text = "Yellow Wins!")
             if ((x < 7 and x > 4 and y < 4)):
                 if(arr[x][y]['text']==arr[x-1][y+1]['text']==arr[x-2][y+2]['text']==arr[x-3][y+3]['text'] and arr[x][y]['text']!= ''):
                     if(arr[x][y]['text'] == "Red"):
+                        playerturn.config(text = "Reset")
                         winLbl.config(text = "Red Wins!")
                     else:
+                        playerturn.config(text = "Reset")
                         winLbl.config(text = "Yellow Wins!")
 
             if ((x > 4 and y > 4) and (x < 7 and y < 7)):
                 if(arr[x][y]['text']==arr[x-1][y-1]['text']==arr[x-2][y-2]['text']==arr[x-3][y-3]['text'] and arr[x][y]['text']!= ''):
                     if(arr[x][y]['text'] == "Red"):
+                        playerturn.config(text = "Reset")
                         winLbl.config(text = "Red Wins!")
                     else:
+                        playerturn.config(text = "Reset")
                         winLbl.config(text = "Yellow Wins!")
             
 
@@ -38,32 +46,43 @@ def press(x,y):
     global yellow_photo
     #red_photo = PhotoImage(file = "Red.png")
     #yellow_photo = PhotoImage(file = "Yellow.png")
-    if (arr[x][y] == arr[6][y]):
-        if (arr[x][y]['text'] != "Red" and arr[x][y]['text'] != "Yellow"):
-            arr[x][y].config(text = player)
-            if player == "Red":
-                arr[x][y].config(image=red_photo)
-                player = "Yellow"
-            else:
-                arr[x][y].config(image=yellow_photo)
-                player = "Red"
-            checkWin()
-    elif (arr[x+1][y]['text'] == "Red" or arr[x+1][y]['text'] == "Yellow"):
-        if (arr[x][y]['text'] != "Red" and arr[x][y]['text'] != "Yellow"):
-            arr[x][y].config(text = player)
-            if player == "Red":
-                arr[x][y].config(image=red_photo)
-                player = "Yellow"
-            else:
-                arr[x][y].config(image=yellow_photo)
-                player = "Red"
-            checkWin()
+    if (winLbl['text'] != "Yellow Wins!" and winLbl['text'] != "Red Wins!"):
+        if (arr[x][y] == arr[6][y]):
+            if (arr[x][y]['text'] != "Red" and arr[x][y]['text'] != "Yellow"):
+                arr[x][y].config(text = player)
+                if player == "Red":
+                    arr[x][y].config(image=red_photo)
+                    player = "Yellow"
+                    playerturn.config(text = "Yellow's turn")
+                    showturn.config(image=yellow_photo)
+                else:
+                    arr[x][y].config(image=yellow_photo)
+                    player = "Red"
+                    playerturn.config(text = "Red's turn")
+                    showturn.config(image=red_photo)
+                checkWin()
+        elif (arr[x+1][y]['text'] == "Red" or arr[x+1][y]['text'] == "Yellow"):
+            if (arr[x][y]['text'] != "Red" and arr[x][y]['text'] != "Yellow"):
+                arr[x][y].config(text = player)
+                if player == "Red":
+                    arr[x][y].config(image=red_photo)
+                    player = "Yellow"
+                    playerturn.config(text = "Yellow's turn")
+                    showturn.config(image=yellow_photo)
+                else:
+                    arr[x][y].config(image=yellow_photo)
+                    player = "Red"
+                    playerturn.config(text = "Red's turn")
+                    showturn.config(image=red_photo)
+                checkWin()
 
 def resetB(arr):
     global photo
     for x in range(cols):
         for y in range(rows):
             arr[x][y].config(image = photo)
+            arr[x][y].config(text = "")
+            winLbl.config(text = "")
 
 # def resetB():
 #     btn1.config(text = '')
@@ -93,7 +112,11 @@ for x in range(cols):
         arr[x][y] = Button(root, text='', command= lambda x1=x, y1=y: press(x1,y1) , height = 30, width = 30, image=photo)
         arr[x][y].grid(row = x, column = y)
 
+showturn = Button(root, text='', height = 30, width = 30, image=red_photo)
+showturn.grid(row = 8, column = 11)
+
 reset = Button(root, text = "Reset", command = lambda: resetB(arr), height = 3, width = 10)
+reset.grid(row = 25, column = 5, columnspan=4)
 ## Create Buttons
 # btn1 = Button(root, text='', command= lambda: press(btn1), height = 3, width = 10)
 # btn2 = Button(root, text='', command= lambda: press(btn2), height = 3, width = 10)
@@ -107,6 +130,7 @@ reset = Button(root, text = "Reset", command = lambda: resetB(arr), height = 3, 
 # reset = Button(root, text = "reset", command = lambda: resetB(), height = 3, width = 10)
 
 winLbl = Label(root)
+playerturn = Label(root, text="Red starts")
 ## Layout Buttons
 # btn1.grid(row = 1, column = 1)
 # btn2.grid(row = 1, column = 2)
@@ -120,5 +144,7 @@ winLbl = Label(root)
 # reset.grid(row = 5, column = 2)
 
 winLbl.grid(row = 8, column = 2, columnspan=3)
+playerturn.grid(row = 11, column=2, columnspan=3)
+
 
 root.mainloop()
