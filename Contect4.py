c5af68ed1557046f2a733aa03f975607f956eb30
PastelKobold <swebster495@gmail.com>
2024-11-22 15:24:43 -0500

    Fix diagonal win condition

--- Contect4.py
+++ Contect4.py
@@ -14,6 +14,7 @@ def checkWin():
                     else:
                         playerturn.config(text = "Reset")
                         winLbl.config(text = "Yellow Wins!")
+                        print("Horizontal")
             if (y < 4):
                 if(arr[x][y]['text']==arr[x][y+1]['text']==arr[x][y+2]['text']==arr[x][y+3]['text'] and arr[x][y]['text']!= ''):
                     if(arr[x][y]['text'] == "Red"):
@@ -22,6 +23,8 @@ def checkWin():
                     else:
                         playerturn.config(text = "Reset")
                         winLbl.config(text = "Yellow Wins!")
+                        print("Vertical")
+            #Diagonals
             if ((x < 7 and x > 4 and y < 4)):
                 if(arr[x][y]['text']==arr[x-1][y+1]['text']==arr[x-2][y+2]['text']==arr[x-3][y+3]['text'] and arr[x][y]['text']!= ''):
                     if(arr[x][y]['text'] == "Red"):
@@ -30,15 +33,17 @@ def checkWin():
                     else:
                         playerturn.config(text = "Reset")
                         winLbl.config(text = "Yellow Wins!")
+                        print("Diagonal right")
 
-            if ((x > 4 and y > 4) and (x < 7 and y < 7)):
-                if(arr[x][y]['text']==arr[x-1][y-1]['text']==arr[x-2][y-2]['text']==arr[x-3][y-3]['text'] and arr[x][y]['text']!= ''):
+            if (x > 4  and (x < 7 and y < 7)):
+                if((arr[x][y]['text']==arr[x-1][y-1]['text']==arr[x-2][y-2]['text']==arr[x-3][y-3]['text']) and arr[x][y]['text']!= '' and  (not x-3 < 0) and (not y-3 < 0)):
                     if(arr[x][y]['text'] == "Red"):
                         playerturn.config(text = "Reset")
                         winLbl.config(text = "Red Wins!")
                     else:
                         playerturn.config(text = "Reset")
                         winLbl.config(text = "Yellow Wins!")
+                        print("Diagonal left")
             
 
 def press(x,y):
@@ -82,13 +87,8 @@ def resetB(arr):
     for x in range(cols):
         for y in range(rows):
             arr[x][y].config(image = photo)
-            arr[x][y].config(text = "")
-            winLbl.config(text = "")
-
-def resetB(arr):
-    for x in range(cols):
-        for y in range(rows):
-            arr[x][y].config(image = '')
+            arr[x][y].config(text = '')
+            winLbl.config(text = '')
 
 root = Tk()
 root.geometry('300x300')
@@ -111,31 +111,9 @@ showturn.grid(row = 8, column = 11)
 
 reset = Button(root, text = "Reset", command = lambda: resetB(arr), height = 3, width = 10)
 reset.grid(row = 25, column = 5, columnspan=4)
-## Create Buttons
-# btn1 = Button(root, text='', command= lambda: press(btn1), height = 3, width = 10)
-# btn2 = Button(root, text='', command= lambda: press(btn2), height = 3, width = 10)
-# btn3 = Button(root, text='', command= lambda: press(btn3), height = 3, width = 10)
-# btn4 = Button(root, text='', command= lambda: press(btn4), height = 3, width = 10)
-# btn5 = Button(root, text='', command= lambda: press(btn5), height = 3, width = 10)
-# btn6 = Button(root, text='', command= lambda: press(btn6), height = 3, width = 10)
-# btn7 = Button(root, text='', command= lambda: press(btn7), height = 3, width = 10)
-# btn8 = Button(root, text='', command= lambda: press(btn8), height = 3, width = 10)
-# btn9 = Button(root, text='', command= lambda: press(btn9), height = 3, width = 10)
-# reset = Button(root, text = "reset", command = lambda: resetB(), height = 3, width = 10)
 
 winLbl = Label(root)
 playerturn = Label(root, text="Red starts")
-## Layout Buttons
-# btn1.grid(row = 1, column = 1)
-# btn2.grid(row = 1, column = 2)
-# btn3.grid(row = 1, column = 3)
-# btn4.grid(row = 2, column = 1)
-# btn5.grid(row = 2, column = 2)
-# btn6.grid(row = 2, column = 3)
-# btn7.grid(row = 3, column = 1)
-# btn8.grid(row = 3, column = 2)
-# btn9.grid(row = 3, column = 3)
-# reset.grid(row = 5, column = 2)
 
 winLbl.grid(row = 8, column = 2, columnspan=3)
 playerturn.grid(row = 11, column=2, columnspan=3)
